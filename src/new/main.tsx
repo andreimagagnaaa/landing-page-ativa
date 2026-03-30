bd0368ed42261e745e49f97dfa44e08d2677275b
clienteativaconsultoria-hub <cliente.ativa.consultoria@gmail.com>
2026-03-30 16:53:02 -0300

    add new /new landing page for MV efficiency campaign

---
+++ src/new/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../index.css';
+import NewLP from './NewLP';
+
+ReactDOM.createRoot(document.getElementById('root-new')!).render(
+  <React.StrictMode>
+    <NewLP />
+  </React.StrictMode>
+);
